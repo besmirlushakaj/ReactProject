b327efd5f730e77c4762a29ad5e26109b27e3933
Besmir Lushakaj <lushakaj.besmir@gmail.com>
2021-01-02 17:19:37 -0500

    commit reacy changes

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md m-1">
+                   
+                </div>
+                <div className="col-md m-1">
+                    
+                </div>
+                <div className="col-md m-1">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;  
